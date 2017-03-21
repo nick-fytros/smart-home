d57f2207f4ce4639b988819a376f0c9183a24dbb
Nick Fytros <fytros.n@gmail.com>
2017-03-21 22:06:24 +0100

    test config with ava

--- test/app.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-var chai   = require('chai');
-
-describe('The app should have test', function () {
-    it('should have tests', function () {
-        assert.equal(true, true);
-    });
-});
