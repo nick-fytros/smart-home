619688fecc926d4c8d3c32ea849737944fb5fc7d
Nick Fytros <fytros.n@gmail.com>
2017-05-28 20:37:51 +0200

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# build the new image
+docker build -t smart-home ~/projects/smart-home
+# stop and rm the previous container
 docker stop smart_home
 docker rm smart_home
-docker rmi smart-home
-docker build -t smart-home ~/projects/smart-home
+# start the new one
 docker run -d -p 6000:6000 --network host --name smart_home smart-home
+# remove dangling images
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
